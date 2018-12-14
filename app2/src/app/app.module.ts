ee77959f70f788ad51606af3713c93537dee087e
amit1007 <a0655216@gmail.com>
2018-12-14 20:01:02 +0530

    Add files via upload

---
+++ app2/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { RegisterComponent } from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AdminComponent } from './admin/admin.component';
+import { FeatureComponent } from './feature/feature.component';
+import { ForgetComponent } from './forget/forget.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+// const appRoutes: Routes = [
+//   {path: '',redirectTo: '/login',pathMatch: 'full'},
+//   {path: 'register',component:RegisterComponent},
+//   {path: 'login',component:LoginComponent},
+//   {path: 'contactus',component:ContactusComponent},
+//   {path: 'admin',component:AdminComponent},
+//   {path: 'aboutus',component:AboutusComponent},
+//   { path: '**', component: PageNotFoundComponent }
+
+// ];
+@NgModule({
+  declarations: [ 
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    AboutusComponent,
+    ContactusComponent,
+    AdminComponent,
+    MyNavComponent,
+    FeatureComponent,
+    ForgetComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    // RouterModule.forRoot(appRoutes ,{ enableTracing: true } ),
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+   ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
